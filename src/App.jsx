d2726706ee3cbfadd8eeee11ab06f98361b5549f
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-01 21:04:33 -0300

    Add more elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 function App() {
   return (
-    <div className="flex flex-wrap flex-row gap-3 h-full w-full">
+    <div className="flex flex-wrap flex-row gap-[3px] h-screen w-full p-[3px]">
         <div className="bg-pink-500 h-[100px] w-full"></div>
-        <div className="flex flex-wrap h-[calc(100vh-206px)] w-full">
-          <div></div>
-          <div></div>
-          <div></div>
+        <div className="flex flex-wrap gap-[3px] h-[calc(100%-206px)] w-full">
+          <div className="bg-pink-500 w-full"></div>
+          <div className="bg-pink-500 w-full"></div>
+          <div className="bg-pink-500 w-full"></div>
         </div>
         <div className="bg-pink-500 h-[100px] w-full"></div>
     </div>
