e18a80f59396478fac43414367a0485e6b879d00
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-02 10:35:51 -0300

    Button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 function App() {
   return (
-    <div className="h-screen w-screen flex flex-col gap-[3px] p-[3px] bg-white">
-      <header className="bg-pink-500 h-[100px] w-full"></header>
-      <main className="flex gap-[3px] h-[calc(100%-206px)] w-full bg-white">
-        <div className="bg-pink-500 h-full" style={{ width: "calc(100% / 3)" }}></div>
-        <div className="bg-pink-500 h-full" style={{ width: "calc(2 * 100% / 3)" }}></div>
-      </main>
-      <footer className="bg-pink-500 h-[100px] w-full"></footer>
-      {/* Optional: Add the button if you want the toggle */}
-      {/* <button className="fixed z-50 bottom-4 right-4 bg-white text-black px-4 py-2 rounded shadow" id="btn">Click</button> */}
-    </div>
+    <>
+      <div className="h-screen w-screen flex flex-col gap-[3px] p-[3px] bg-white">
+        <header className="bg-pink-500 h-[100px] w-full"></header>
+        <main className="flex gap-[3px] h-[calc(100%-206px)] w-full bg-white">
+          <div className="bg-pink-500 h-full" style={{ width: "calc(100% / 3)" }}></div>
+          <div className="bg-pink-500 h-full" style={{ width: "calc(2 * 100% / 3)" }}></div>
+        </main>
+        <footer className="bg-pink-500 h-[100px] w-full"></footer>
+      </div>
+      <button className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition duration-300">
+        Click Me
+      </button>
+    </>
   )
 }
 
