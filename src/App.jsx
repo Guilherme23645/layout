3a57f650d58085a5ef5340acb3416afe56c6e232
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-02 11:36:39 -0300

    README description refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
 function App() {
   return (
     <>
-      <div className="h-screen w-screen flex flex-col gap-[3px] p-[3px] bg-white">
-        <header className="bg-pink-500 h-[100px] w-full"></header>
-        <main className="flex gap-[3px] h-[calc(100%-206px)] w-full bg-white">
-          <div className="bg-pink-500 h-full" style={{ width: "calc(100% / 3)" }}></div>
-          <div className="bg-pink-500 h-full" style={{ width: "calc(2 * 100% / 3)" }}></div>
-        </main>
-        <footer className="bg-pink-500 h-[100px] w-full"></footer>
+      <div class="h-screen flex flex-wrap flex-row gap-[3px] p-[3px]">
+    <div class="h-[100px] w-full bg-amber-300">
+
+    </div>
+    <div class="flex gap-[3px] flex-col h-[calc(100%-206px)] w-full">
+      <div class="h-full flex flex-row gap-[3px]">
+        <div class="h-full bg-amber-300 flex-1"></div>
+        <div class="h-full bg-amber-300 flex-2"></div>
       </div>
-      <button className="bg-blue-500 text-white p-2 rounded hover:bg-blue-600 transition duration-300">
-        Click Me
-      </button>
+      <div class="h-full flex flex-row gap-[3px]">
+        <div class="h-full bg-amber-300 flex-3"></div>
+        <div class="h-full bg-amber-300 flex-1"></div>
+      </div>
+    </div>
+    <div class="h-[100px] w-full bg-amber-300">
+
+    </div>
+  </div>
     </>
   )
 }
