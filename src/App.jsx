b0c3bc4469e6d5993468808c04e21db81121d1b2
Guilherme23645 <gui.depaula236@gmail.com>
2025-07-05 02:09:12 -0300

    Further playing with layouts and colors

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 function App() {
   return (
     <>
-      <div class="h-screen flex flex-wrap flex-row gap-[3px] p-[3px]">
-    <div class="h-[100px] w-full bg-amber-300">
+      <div class="h-screen flex flex-col gap-[3px] p-[3px]">
+    <div class="h-[100px] w-full bg-[lightgreen]">
 
     </div>
     <div class="flex gap-[3px] flex-col h-[calc(100%-206px)] w-full">
       <div class="h-full flex flex-row gap-[3px]">
-        <div class="h-full bg-amber-300 flex-4"></div>
-        <div class="h-full bg-amber-300 flex-3"></div>
+        <div class="h-full bg-[orange] flex-5"></div>
+        <div class="h-full bg-[tomato] flex-3"></div>
       </div>
       <div class="h-full flex flex-row gap-[3px]">
-        <div class="h-full bg-amber-300 flex-3"></div>
-        <div class="h-full bg-amber-300 flex-1"></div>
+        <div class="h-full bg-[aquamarine] flex-3"></div>
+        <div class="h-full bg-[yellow] flex-1"></div>
       </div>
     </div>
-    <div class="h-[100px] w-full bg-amber-300">
+    <div class="h-[100px] w-full bg-[pink]">
 
     </div>
   </div>
